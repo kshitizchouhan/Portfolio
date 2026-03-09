4e1118e7e83f4e888688141ed93def3c053b67c8
Kshitiz Chouhan <chouhankshitiz25@gmail.com>
2026-03-09 18:35:01 +0530

    updated contact email system using resend

--- portfolio-backend/src/utils/sendMail.ts
+++ portfolio-backend/src/utils/sendMail.ts
@@ -1,34 +1,31 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-import nodemailer from "nodemailer";
+import { Resend } from "resend";
 
-const transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS
-  }
-});
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 const sendMail = async (name: string, email: string, message: string) => {
+  try {
 
-  await transporter.sendMail({
-    from: process.env.EMAIL_USER,
-    to: process.env.EMAIL_USER,
-    subject: "New Portfolio Message",
-    text: `
+    // Email to you
+    await resend.emails.send({
+      from: "Portfolio <onboarding@resend.dev>",
+      to: process.env.EMAIL_USER!,
+      subject: "New Portfolio Message",
+      text: `
 Name: ${name}
 Email: ${email}
 Message: ${message}
 `
-  });
+    });
 
-  await transporter.sendMail({
-    from: process.env.EMAIL_USER,
-    to: email,
-    subject: "Thanks for contacting me",
-    text: `
+    // Auto reply to user
+    await resend.emails.send({
+      from: "Kshitiz Portfolio <onboarding@resend.dev>",
+      to: email,
+      subject: "Thanks for contacting me",
+      text: `
 Hi ${name},
 
 Thank you for contacting me through my portfolio website.
@@ -38,8 +35,13 @@ I have received your message and will reply soon.
 Best regards,
 Kshitiz Chouhan
 `
-  });
+    });
 
+    console.log("Emails sent successfully");
+
+  } catch (error) {
+    console.error("Resend email error:", error);
+  }
 };
 
 export default sendMail;
